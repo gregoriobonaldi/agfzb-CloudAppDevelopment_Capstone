86a19b27ac23b19f05a6b90f116e88b3110b2fe6
gregoriobonaldi <gregorio.bonaldi@gmail.com>
2022-05-19 09:59:22 +0000

    final project parte 2b fix

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost", "gregoriobonaldi-djangoapp"]
+ALLOWED_HOSTS = ["localhost", "gregoriobonaldi-djangoapp.eu-de.mybluemix.net"]
 
 
 # Application definition
